81521c5a92948001cd7b1abaa9b4a949a74625f0
Midnight000 <873501007@qq.com>
2025-04-03 16:09:28 +0800

    combine PCNet* and inpainting

--- src/python/My_PCNet/train_PCNet.py
+++ src/python/My_PCNet/train_PCNet.py
@@ -3,7 +3,6 @@ from os.path import join, abspath
 import argparse
 
 # set which GPU(s) to use
-os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3'
 
 from src.python.My_PCNet.utils import print_sys_info, set_torch_reproducibility
 from src.python.My_PCNet.My_train_network import train_eval_pcnet, get_model_train_cfg
@@ -18,6 +17,8 @@ parser = argparse.ArgumentParser(description="示例：Python 脚本参数")
 parser.add_argument('--device', nargs='+', type=int, default=[0], help="指定使用的 GPU 设备列表，如 0 1 2")
 
 args = parser.parse_args()
+gpu_list = ",".join(map(str, args.device))  # 转换为字符串 "0,1,2"
+os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
 # training configs
 data_root = abspath(join(os.getcwd(), '../../../data'))
 setup_list = [
@@ -44,5 +45,5 @@ setup_list = [
 
 
 # pcnet_cfg       = get_model_train_cfg(['PCNet'], data_root, setup_list, load_pretrained=False, plot_on=True)
-pcnet_cfg       = get_model_train_cfg(['My_PCNet_no_mask'], data_root, setup_list, device_ids=args.device, center_crop=True, load_pretrained=False, plot_on=True)
+pcnet_cfg       = get_model_train_cfg(['My_PCNet_no_mask'], data_root, setup_list, center_crop=True, load_pretrained=False, plot_on=True)
 _, pcnet_ret, _ = train_eval_pcnet(pcnet_cfg)
